68a29a8606a560a1f4a1be9d62ae7505fc29908e
Pedro Henrique <pedromonteiromm@gmail.com>
2025-03-15 19:54:43 -0300

    Ajustando caminho imgs no arquivo script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const menuHamburger = document.querySelector(".header-menu-hamburguer");
 const imageMh = document.querySelector(".image-mh");
 const menuDropdown = document.querySelector(".container-dropmenu")
 
-const mhIcon = "/img/icons/menu_24dp_1F1F1F_FILL0_wght400_GRAD0_opsz24.svg";
-const closeIcon = "/img/icons/close_24dp_000000_FILL0_wght400_GRAD0_opsz24.svg";
+const mhIcon = "./img/icons/menu_24dp_1F1F1F_FILL0_wght400_GRAD0_opsz24.svg";
+const closeIcon = "./img/icons/close_24dp_000000_FILL0_wght400_GRAD0_opsz24.svg";
 
 
 menuHamburger.addEventListener('click', (e) => {
