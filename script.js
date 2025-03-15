af1e911f7643f18a178b6f2a8612dacbff4cd15b
Pedro Henrique <pedromonteiromm@gmail.com>
2025-03-15 19:49:55 -0300

    Trabalhando no menuhamburguer

--- script.js
+++ script.js
@@ -8,24 +8,26 @@ const closeIcon = "/img/icons/close_24dp_000000_FILL0_wght400_GRAD0_opsz24.svg";
 
 menuHamburger.addEventListener('click', (e) => {
 
+    
     imageMh.style.transition = "opacity 0.5s ease";
     imageMh.style.opacity = "0";
-    
+     
 
     setTimeout(() => {
-        
-        if (imageMh.getAttribute("src") === mhIcon) {
-            imageMh.setAttribute("src", closeIcon);
-        } else {
-            imageMh.setAttribute("src", mhIcon);
-        }
 
+          if(imageMh.getAttribute("src") === mhIcon) {
+        imageMh.setAttribute("src", closeIcon)
+    } else {
+        imageMh.setAttribute("src", mhIcon)
+    }
+      
         setTimeout(() => {
             imageMh.style.opacity = "1";
         }, 300);
     }, 100);
 
     menuDropdown.classList.toggle('hidden')
- 
+    document.body.classList.toggle('no-overflow')
+
 });
 
