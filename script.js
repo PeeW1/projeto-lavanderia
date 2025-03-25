e3fb24493408d89207bea9d81bc29ee970677454
Pedro Henrique <pedromonteiromm@gmail.com>
2025-03-25 14:43:32 -0300

    Ajustanto pagina sobre e servicos

--- script.js
+++ script.js
@@ -5,12 +5,10 @@ document.addEventListener("DOMContentLoaded", () => {
     const menusCarrocel = document.querySelectorAll(".menus-carrocel")
     const menuNav = document.querySelector(".menu-nav");
     const arrowLeft = document.querySelectorAll(".img-arrow-left");
-const arrowRight = document.querySelectorAll(".img-arrow-right");
+    const arrowRight = document.querySelectorAll(".img-arrow-right");
+    const dots = document.querySelectorAll(".dot");
 
-    console.log(menusCarrocel.length) 
 
-    console.log(arrowLeft, arrowRight)
-    
 
 
     const mhIcon = "./img/icons/menu_24dp_1F1F1F_FILL0_wght400_GRAD0_opsz24.svg";
@@ -20,8 +18,7 @@ const arrowRight = document.querySelectorAll(".img-arrow-right");
     
     function showMenu(index) {
         menusCarrocel.forEach(menu => menu.classList.remove("active"))
-        menusCarrocel[index].classList.add("active")  
-        console.log("Menu Atual: " + index);  
+        menusCarrocel[index].classList.add("active");
     }
 
     arrowRight.forEach(arrow => {
@@ -45,6 +42,16 @@ const arrowRight = document.querySelectorAll(".img-arrow-right");
             showMenu(currentMenuIndex);
         });
     });
+
+    dots[0].addEventListener('click', (e) => {
+        menusCarrocel[0].classList.toggle("active")
+    })
+    dots[1].addEventListener('click', (e) => {
+        menusCarrocel[1].classList.toggle("active")
+    })
+    dots[2].addEventListener('click', (e) => {
+        menusCarrocel[2].classList.toggle("active")
+    })
 
     
     menuHamburger.addEventListener('click', (e) => {
